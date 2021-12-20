5314505ded825b5dffde080a42caf653ebcef0e7
Santiago Oliveira <santioliveirasgo@gmail.com>
2021-12-20 17:26:16 -0300

    asd

--- src/main/java/com/example/demo/MimacomApplication.java
+++ src/main/java/com/example/demo/MimacomApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MimacomApplication {
 
 	
-	//tests
+	//test s
 	
 	public static void main(String[] args) {
 		SpringApplication.run(MimacomApplication.class, args);
